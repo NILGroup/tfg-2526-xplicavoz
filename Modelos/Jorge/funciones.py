3fcc808759f2b8d83aab3a8a82eae7631bd88d75
Jorgexe12 <jaured@ucm.es>
2026-04-14 21:12:22 +0200

    cambios en la memoria y en XAI

--- Modelos/Jorge/funciones.py
+++ Modelos/Jorge/funciones.py
@@ -3,6 +3,7 @@ from sklearn.metrics import (accuracy_score, precision_score, recall_score,
                            roc_auc_score, roc_curve, matthews_corrcoef)
 import matplotlib.pyplot as plt
 import seaborn as sns
+from IPython.core.display import HTML
 
 from sklearn.ensemble import RandomForestClassifier
 import lightgbm as lgbm
@@ -11,10 +12,13 @@ from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 
 import shap
+from lime.lime_tabular import LimeTabularExplainer
+import dice_ml
 
 import numpy as np
 import pandas as pd
 import json
+import re
 
 def carga_datos(ruta_bon, ruta_spo, random_state):
     df_bonafide = pd.read_csv(ruta_bon)
@@ -181,46 +185,72 @@ def evaluacion_modelo(model_name: str, y_true, y_pred, y_pred_proba,
     }
     
 # FUNCIONES XAI
-def show(model, i, shap_values, save=False):
+def show(model, i, shap_values):
     for j in range(i):
-        if save:
-            # Para waterfall plots necesitamos capturar la figura de SHAP
-            shap.waterfall_plot(shap_values[j], show=False)
-            
-            # Obtener la figura actual y guardarla
-            fig = plt.gcf()
-            fig.savefig(f'shap_local_{j}.png', bbox_inches='tight', dpi=300)
-            plt.close()
-        else:
-            model(shap_values[j])
+        model(shap_values[j])
         
-def explica_shap(modelo, X_train, X_test, n_show, local, save=False, ret=False):
+def explica_shap(modelo, X_train, X_test, n_show, local):
     # Generamos explicaciones
     explainer = shap.Explainer(modelo.predict, X_train)
     shap_values = explainer(X_test)
     
     # Mostramos los valores shap dependiendo de si es explicación local o global
     if local:
-        show(model=shap.plots.waterfall, i=n_show, shap_values=shap_values, save=save)
+        show(model=shap.plots.waterfall, i=n_show, shap_values=shap_values)
     else:
         plt.figure(figsize=(12, 8))
-        
-        if save:
-            shap.summary_plot(shap_values, X_test)
-            fig = plt.gcf()
-            fig.savefig('shap_global.png', bbox_inches='tight', dpi=300)
-            fig.close()
-        else:
-            shap.summary_plot(shap_values, X_test)
-            plt.show()
+        shap.summary_plot(shap_values, X_test)
+        plt.show()
     
-    # Por último devolvemos y_pred por si evaluamos el modelo
-    y_pred = modelo.predict(X_test)
-    y_pred_proba = modelo.predict_proba(X_test)[:, 1]
-    if ret: return y_pred, y_pred_proba
+def explica_lime(modelo, columnas, X_train, X_test):
+    explainer = LimeTabularExplainer(X_train.values, feature_names=columnas, class_names=['label'], mode='classification')
+    exp = explainer.explain_instance(X_test.values[0], modelo.predict_proba)
 
-def drop_features_explica_shap(modelo, model_name, X_train, y_train, X_test, y_test, feats, n_show, 
-                               evalua_modelo=False, local=True, save=False):
+
+    # Obtenemos el HTML generado por LIME
+    exp_html = exp.as_html()
+
+
+    # Removemos las etiquetas <style> internas (que pueden estar forzando otros colores, para intentar quitar el formato por defecto)
+    exp_html_clean = re.sub(r'<style.*?>.*?</style>', '', exp_html, flags=re.DOTALL)
+
+    # Envolvemos el HTML limpio en un contenedor 
+    # también aplicamos un estilo global para forzar todo el contenido a tener fondo blanco
+    html_con_style = f"""
+    <style>
+    .lime-container * {{
+        color: black !important;
+        background-color: white !important;
+        border-color: black !important;
+    }}
+    </style>
+    <div class="lime-container">
+        {exp_html_clean}
+    </div>
+    """
+    
+    return HTML(html_con_style)
+
+def explica_dice(modelo, X_train, y_train, X_test, escalar=False):
+    if escalar:
+        scaler = StandardScaler()
+        X_train = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns, index=X_train.index)
+        X_test = pd.DataFrame(scaler.transform(X_test), columns=X_test.columns, index=X_test.index)
+    # Combina features con target para DiCE
+    data_dice = X_train.copy()
+    data_dice['label'] = y_train
+    
+    # Define el DataInterface
+    dice_data = dice_ml.Data(dataframe=data_dice, continuous_features=X_train.columns.tolist(), outcome_name='label')
+    dice_model = dice_ml.Model(model=modelo, backend='sklearn')
+    exp = dice_ml.Dice(dice_data, dice_model, method="random")
+    
+    query = X_test.iloc[[0]]
+    e1 = exp.generate_counterfactuals(query_instances=query, total_CFs=3, desired_class="opposite")
+    e1.visualize_as_dataframe(show_only_changes=True)
+
+def drop_features_explica(modelo, model_name, X_train, y_train, X_test, y_test, feats, n_show, 
+                               explicacion, evalua_modelo=False, local=True, cols_lime=[]):
     # Creamos los nuevos conjuntos de train y test
     X_train_new = X_train.drop(feats, axis=1, inplace=False)
     X_test_new = X_test.drop(feats, axis=1, inplace=False)
@@ -239,8 +269,15 @@ def drop_features_explica_shap(modelo, model_name, X_train, y_train, X_test, y_t
         ])
     modelo.fit(X_train_new, y_train)
     
-    # Generamos las explicaciones como antes
-    y_pred, y_pred_proba = explica_shap(modelo=modelo, X_train=X_train_new, X_test=X_test_new, n_show=n_show, local=local, save=save, ret=True)
+    # Generamos las explicaciones
+    ret = None
+    if explicacion == 'shap': explica_shap(modelo=modelo, X_train=X_train_new, X_test=X_test_new, n_show=n_show, local=local)
+    elif explicacion == 'lime': ret = explica_lime(modelo=modelo, columnas=cols_lime, X_train=X_train_new, X_test=X_test_new)
     
     # Evaluamos el modelo
-    if evalua_modelo: evaluacion_modelo(model_name=model_name, y_true=y_test, y_pred=y_pred, y_pred_proba=y_pred_proba)
+    if evalua_modelo: 
+        y_pred = modelo.predict(X_test_new)
+        y_pred_proba = modelo.predict_proba(X_test_new)[:, 1]
+        evaluacion_modelo(model_name=model_name, y_true=y_test, y_pred=y_pred, y_pred_proba=y_pred_proba)
+        
+    if ret: return ret
