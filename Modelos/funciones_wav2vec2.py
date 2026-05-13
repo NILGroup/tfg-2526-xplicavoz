6eb684021b55288513044a1f5cb263ad8dd3e053
IvanPastorSacristan <162611785+IvanPastorSacristan@users.noreply.github.com>
2026-05-13 13:27:25 +0200

    Mas pipeline

--- Modelos/funciones_wav2vec2.py
+++ Modelos/funciones_wav2vec2.py
@@ -59,6 +59,8 @@ import shap
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 import os
+import re
+from lime import lime_tabular
 
 TARGET_TIME = 128
 
@@ -197,7 +199,7 @@ def mostrar_shap(espectrograma, shap, audio,ax):
 
 # IG
 
-def integrated_gradients(model, audio, baseline=None, steps=50):
+def explicacion_integrated_gradients(model, audio, baseline=None, steps=50):
     
     x = extract_spectrogram(audio)
     x = fix_spec(x,target_time=TARGET_TIME)
@@ -242,19 +244,168 @@ def mostrar_ig(espectrograma,ig,audio,ax):
 
     return im
 
+def explicacion_lime(model,audio,X,num_features = 512):
+    # Dataset de entrenamiento
+    X_np = X.numpy().astype(np.float32)
+    X_np = np.nan_to_num(X_np)
+
+    # Extraer espectrograma del audio
+    mel_spec = extract_spectrogram(audio)
+    mel_spec = fix_spec(mel_spec,target_time=128)
+
+    # Normalización
+    mel_spec = (mel_spec - X_np.mean()) / (X_np.std() + 1e-8)
+    mel_spec = np.nan_to_num(mel_spec)
+
+    n_samples, n_mels, n_frames, _ = X_np.shape
+
+    # Flatten dataset entrenamiento
+    X_flat = X_np[..., 0].reshape(n_samples,-1)
+
+    # Audio a explicar
+    x_sample = mel_spec.reshape(-1)
+
+    # Función de predicción
+    def predict_fn_lime_flat(x_flat):
+
+        x_reshaped = x_flat.reshape(-1,n_mels,n_frames,1).astype(np.float32)
+        x_reshaped = np.nan_to_num(x_reshaped)
+
+        preds = model.predict(x_reshaped,verbose=0)
+
+        return preds
+    
+    # Feature names
+    feature_names = [f"mel{i}_t{t}"for i in range(n_mels)for t in range(n_frames)]
+
+    # Crear explainer
+    explainer = lime_tabular.LimeTabularExplainer(training_data=X_flat,feature_names=feature_names,class_names=["score"],mode="regression")
+
+    # Explicación LIME
+    exp_lime = explainer.explain_instance(data_row=x_sample,predict_fn=predict_fn_lime_flat,num_features=num_features)
+
+    # Convertir explicación a mapa 2D
+    lime_map = np.zeros((n_mels, n_frames),dtype=np.float32)
+
+    feature_list = exp_lime.as_list()
+
+    for feature_name, weight in feature_list:
+
+        match = re.search(r'mel(\d+)_t(\d+)',feature_name)
+
+        if match:
+            mel_idx = int(match.group(1))
+            time_idx = int(match.group(2))
+            lime_map[mel_idx, time_idx] = weight
+
+    # Evitar mapas constantes
+    if np.isclose(lime_map.std(), 0):
+        lime_map += np.random.normal(0,1e-6,lime_map.shape)  
+
+    # Normalización [0,1]
+    min_val = lime_map.min()
+
+    max_val = lime_map.max()
+
+    lime_map = (lime_map - min_val) / (max_val - min_val + 1e-8 )
+
+    return lime_map
+
+
+def mostrar_lime(espectrograma,lime,audio,ax):       
+    im = ax.imshow(lime,aspect='auto',origin='lower',cmap='coolwarm',alpha=1,extent=[0, espectrograma.shape[1], 0, 64])
+
+    ax.set_yticks(list(range(0, 65, 10)))
+    ax.set_xlabel('Time frames')
+    ax.set_ylabel('Mel bands')
+    ax.set_title(f'LIME - {os.path.splitext(os.path.basename(audio))[0]}')
+
+    return im
+
+def explicacion_gc(model,audio,last_conv_layer_name="conv"):
+
+    # Extraer espectrograma audio
+    mel_spec = extract_spectrogram(audio)
+    mel_spec = fix_spec(mel_spec,target_time=128)
+
+    # Normalización
+    mel_spec = mel_spec.astype(np.float32)
+
+    mel_spec = (mel_spec - np.mean(mel_spec)) / (np.std(mel_spec) + 1e-8)
+
+    mel_spec = np.nan_to_num(mel_spec)
+
+    # Input modelo
+    img = mel_spec[np.newaxis, ..., np.newaxis]
+
+    # Modelo GradCAM
+    grad_model = tf.keras.models.Model(
+        inputs=model.inputs,
+        outputs=[model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    # Gradientes
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(img)
+        loss = predictions[:, 0]  # regresión
+
+    grads = tape.gradient(loss, conv_outputs)
+
+    # Comprobar gradientes nulos
+    if grads is None:
+
+        raise ValueError(
+            "Gradientes None. "
+            "Comprueba que la capa conv es correcta."
+        )
+    
+    # Pooling gradientes
+    pooled_grads = tf.reduce_mean(grads, axis=(0,1,2))
+    conv_outputs = conv_outputs[0]
+
+    # Heatmap
+    heatmap = tf.reduce_sum(conv_outputs * pooled_grads, axis=-1)
+
+    # Evitar heatmap todo cero
+    heatmap = tf.abs(heatmap)
+    heatmap = heatmap.numpy()
+
+    # Evitar mapa constante
+    if np.isclose(heatmap.std(), 0):
+        heatmap += np.random.normal(0,1e-6,heatmap.shape)
+
+    # Normalización
+    heatmap = (heatmap - heatmap.min()) / (heatmap.max() - heatmap.min() + 1e-8)
+
+    return heatmap
+
+def mostrar_gc(espectrograma,gc,audio,ax):
+    im = ax.imshow(gc,aspect='auto',origin='lower',cmap='coolwarm',alpha=1,extent=[0, espectrograma.shape[1], 0, 64])
+
+    ax.set_yticks(list(range(0, 65, 10)))
+    ax.set_xlabel('Time frames')
+    ax.set_ylabel('Mel bands')
+    ax.set_title(f'GC - {os.path.splitext(os.path.basename(audio))[0]}')
+
+    return im
+
 def mostrar_graficas(model,audio,X_tensor):
     mel_spec = extract_spectrogram(audio)  # (64, tiempo)
 
     shap = explicacion_shap(model,audio,X_tensor)
 
-    ig = integrated_gradients(model, audio, steps=100)
+    ig = explicacion_integrated_gradients(model, audio, steps=100)
+
+    lime = explicacion_lime(model,audio,X_tensor)
+
+    gc = explicacion_gc(model,audio)
 
     n_frames = mel_spec.shape[1]
 
     shap = shap[:, :n_frames]
     ig = ig[:, :n_frames]
 
-    fig, axes = plt.subplots(1, 3, figsize=(18, 5))
+    fig, axes = plt.subplots(1, 5, figsize=(25, 8))
 
     # 1. Mel
     axes[0].imshow(mel_spec, aspect='auto', origin='lower', cmap='magma')
@@ -265,6 +416,13 @@ def mostrar_graficas(model,audio,X_tensor):
 
     # 3. IG
     im_ig = mostrar_ig(mel_spec, ig, audio, axes[2])
+
+    # 4. LIME
+    im_lime = mostrar_lime(mel_spec,lime,audio,axes[3])
+
+    # 5. GC
+
+    im_gc = mostrar_gc(mel_spec,gc,audio,axes[4])
 
     plt.tight_layout()
     plt.show()
